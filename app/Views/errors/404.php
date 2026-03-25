40af0f6425559fa518f95f0e2017bd7eddc7cf34
AlAlifspecialist <alalifsarker@gmail.com>
2026-03-25 22:50:15 +0100

    Delete app directory

--- app/Views/errors/404.php
+++
@@ -1,5 +0,0 @@
-<div class="text-center py-5">
-    <h1 class="display-5 fw-bold">404</h1>
-    <p class="lead">The page or programme you requested was not found.</p>
-    <a class="btn btn-primary" href="<?= e(url()) ?>">Back to home</a>
-</div>
