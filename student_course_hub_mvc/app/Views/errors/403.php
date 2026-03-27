ae8473a89822ba3bdc2771c6e87fc04a17747d6a
AlAlifspecialist <alalifsarker@gmail.com>
2026-03-27 01:05:21 +0100

    Delete student_course_hub_mvc directory

--- student_course_hub_mvc/app/Views/errors/403.php
+++
@@ -1,5 +0,0 @@
-<div class="text-center py-5">
-    <h1 class="display-5 fw-bold">403</h1>
-    <p class="lead">You do not have permission to access this area.</p>
-    <a class="btn btn-primary" href="<?= e(url()) ?>">Back to home</a>
-</div>
